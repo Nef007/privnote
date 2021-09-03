aacc5ff0259993b758d2d2533c87967e5fbd56c3
Vladislav <50168465+Nef007@users.noreply.github.com>
2021-09-03 20:57:33 +0300

    изменил таймер

--- client/src/compoments/Timer.js
+++ client/src/compoments/Timer.js
@@ -1,6 +1,6 @@
 
 import * as React from 'react';
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import {useTranslation} from "react-i18next";
 
 
@@ -10,12 +10,42 @@ export const Timer = (props) => {
        sec
     } = props
 
+    useEffect( () => {
+        start(sec)
+    },[])
+
     const [time, setTime] = useState("")
+
     timer(sec)
 
+    function start (endTime) {
+        let now = new Date().getTime();
+        let diff = endTime - now;
+        if (diff > 0) {
+            let day = Math.floor(diff / (60 * 60) / 1000 / 24);
+            let hour = Math.floor(diff / (60 * 60) / 1000);
+            let minutes = Math.floor(diff % (1000 * 60 * 60) / (1000 * 60));
+            let seconds = Math.floor(diff % (1000 * 60) / 1000);
+
+            if (day) {
+                setTime(day + `${t("day")}`);
+            } else if (hour) {
+                setTime(hour + `${t("hour")}` + minutes + `${t("min")}`);
+            } else {
+                setTime(minutes + `${t("min_and")}` + seconds + `${t("ces")}`);
+            }
+
+
+            // minutes = minutes < 10 ? `0${minutes}` : minutes;
+            // seconds = seconds < 10 ? `0${seconds}` : seconds;
+
+        } else {
+            setTime("0")
+        }
+    }
 
     function timer(endTime) {
-        var myTimer = setInterval(function () {
+        var myTimer = setInterval(function  () {
             let now = new Date().getTime();
             let diff = endTime - now;
             if (diff > 0) {
